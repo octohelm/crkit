37edd743a89434be1aca0589e1c375bd2fda92d4
Morlay <morlay.null@gmail.com>
2024-08-13 18:59:55 +0800

    feat: added executable oci supported

---
+++ pkg/executable/packer.go
@@ -0,0 +1,171 @@
+package executable
+
+import (
+	"context"
+	"github.com/containerd/containerd/images"
+	"github.com/containerd/platforms"
+	containerregistryv1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/empty"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/partial"
+	"github.com/octohelm/crkit/pkg/artifact"
+	kubepkgv1alpha1 "github.com/octohelm/kubepkgspec/pkg/apis/kubepkg/v1alpha1"
+	"github.com/octohelm/kubepkgspec/pkg/workload"
+	specv1 "github.com/opencontainers/image-spec/specs-go/v1"
+	"io"
+)
+
+const ArtifactType = "application/vnd.executable+type"
+const MediaTypeBinaryContent = "application/vnd.executable"
+
+func PlatformedBinary(platform string, open func() (io.ReadCloser, error)) (LayerWithPlatform, error) {
+	p, err := platforms.Parse(platform)
+	if err != nil {
+		return nil, err
+	}
+
+	l, err := artifact.FromOpener(MediaTypeBinaryContent, open)
+	if err != nil {
+		return nil, err
+	}
+
+	return &layerWithPlatform{
+		Layer:    l,
+		platform: p,
+	}, nil
+}
+
+type layerWithPlatform struct {
+	artifact.Layer
+	platform specv1.Platform
+}
+
+func (l layerWithPlatform) Platform() *containerregistryv1.Platform {
+	return &containerregistryv1.Platform{
+		OS:           l.platform.OS,
+		Architecture: l.platform.Architecture,
+		Variant:      l.platform.Variant,
+		OSVersion:    l.platform.OSVersion,
+	}
+}
+
+type LayerWithPlatform interface {
+	artifact.Layer
+	Platform() *containerregistryv1.Platform
+}
+
+type Packer struct {
+}
+
+func (p *Packer) PackAsIndexOfOciTar(ctx context.Context, imageName string, layers ...LayerWithPlatform) (containerregistryv1.ImageIndex, error) {
+	image := workload.ParseImage(imageName)
+
+	index, err := p.PackAsIndex(ctx, layers...)
+	if err != nil {
+		return nil, err
+	}
+
+	add := mutate.IndexAddendum{
+		Add: index,
+	}
+
+	d, err := partial.Descriptor(index)
+	if err != nil {
+		return nil, err
+	}
+	add.Descriptor = *d
+
+	if add.Annotations == nil {
+		add.Annotations = map[string]string{}
+	}
+
+	add.Annotations[specv1.AnnotationBaseImageName] = image.Name
+
+	if image.Tag != "" {
+		add.Annotations[specv1.AnnotationRefName] = image.Tag
+	}
+
+	if add.ArtifactType == "" {
+		add.Annotations[images.AnnotationImageName] = image.FullName()
+	}
+
+	return mutate.AppendManifests(empty.Index, add), nil
+}
+
+func (p *Packer) PackAsIndex(ctx context.Context, layers ...LayerWithPlatform) (containerregistryv1.ImageIndex, error) {
+	var index containerregistryv1.ImageIndex = empty.Index
+
+	for _, layer := range layers {
+
+		img, err := p.packAsImage(ctx, layer)
+		if err != nil {
+			return nil, err
+		}
+
+		d, err := partial.Descriptor(img)
+		if err != nil {
+			return nil, err
+		}
+
+		add := mutate.IndexAddendum{
+			Add:        img,
+			Descriptor: *d,
+		}
+		add.Platform = layer.Platform()
+
+		index = mutate.AppendManifests(index, add)
+	}
+
+	return index, nil
+}
+
+func (p *Packer) packAsImage(ctx context.Context, layer LayerWithPlatform) (containerregistryv1.Image, error) {
+	i := empty.Image
+
+	gzipped, err := artifact.Gzipped(layer)
+	if err != nil {
+		return nil, err
+	}
+
+	i, err = mutate.AppendLayers(i, gzipped)
+	if err != nil {
+		return nil, err
+	}
+
+	return artifact.Artifact(i, artifact.EmptyConfig(ArtifactType))
+}
+
+func (p *Packer) appendManifests(idx containerregistryv1.ImageIndex, source partial.Describable, desc *containerregistryv1.Descriptor, image *kubepkgv1alpha1.Image) (containerregistryv1.ImageIndex, error) {
+	if desc == nil {
+		d, err := partial.Descriptor(source)
+		if err != nil {
+			return nil, err
+		}
+		desc = d
+	}
+
+	add := mutate.IndexAddendum{
+		Add:        source,
+		Descriptor: *desc,
+	}
+
+	if image != nil {
+		if add.Annotations == nil {
+			add.Annotations = map[string]string{}
+		}
+
+		if image.Name != "" {
+			add.Annotations[specv1.AnnotationBaseImageName] = image.Name
+
+			if add.ArtifactType == "" {
+				add.Annotations[images.AnnotationImageName] = image.FullName()
+			}
+		}
+
+		if image.Tag != "" {
+			add.Annotations[specv1.AnnotationRefName] = image.Tag
+		}
+	}
+
+	return mutate.AppendManifests(idx, add), nil
+}
---
+++ pkg/executable/packer_test.go
@@ -0,0 +1,41 @@
+package executable
+
+import (
+	"context"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/octohelm/crkit/pkg/ocitar"
+	testingx "github.com/octohelm/x/testing"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestPacker(t *testing.T) {
+	p := &Packer{}
+
+	t.Run("should pack as index", func(t *testing.T) {
+		amd64Bin, err := PlatformedBinary("linux/amd64", func() (io.ReadCloser, error) {
+			return os.Open("../../target/crkit_linux_amd64/crkit")
+		})
+		testingx.Expect(t, err, testingx.BeNil[error]())
+		arm64Bin, err := PlatformedBinary("linux/arm64", func() (io.ReadCloser, error) {
+			return os.Open("../../target/crkit_linux_arm64/crkit")
+		})
+		testingx.Expect(t, err, testingx.BeNil[error]())
+
+		idx, err := p.PackAsIndexOfOciTar(context.Background(), "docker.io/x/crkit:v0", amd64Bin, arm64Bin)
+		testingx.Expect(t, err, testingx.BeNil[error]())
+
+		err = writeAsOciTar("../../target/bin.oci.tar", idx)
+		testingx.Expect(t, err, testingx.BeNil[error]())
+	})
+}
+
+func writeAsOciTar(filename string, idx v1.ImageIndex) error {
+	f, err := os.OpenFile(filename, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return ocitar.Write(f, idx)
+}
